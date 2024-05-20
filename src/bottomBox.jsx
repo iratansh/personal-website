659ae8b965cc74eef0ca197ad544f8f267975b57
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-05-20 15:43:45 -0600

    Update bottomBox.jsx

--- src/bottomBox.jsx
+++ src/bottomBox.jsx
@@ -6,7 +6,6 @@ export default function BottomBox() {
         <div className="end">
           <div className="bottomContainer">
             <div className="box6">
-              <p className="websiteName">ISHAAN'S WEBSITE</p>
             </div>
           </div>
         </div>
